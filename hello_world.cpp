3b9883428a0115d64547fb5324729ddfa44cacc7
Hidezen <166001523+Hidezen@users.noreply.github.com>
2025-04-08 13:21:03 +0300

    hello_world.cpp
    just a simple program

---
+++ hello_world.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+void helloWorld() {
+    std::cout << "Hello, World!" << std::endl;
+}
+
+int main() {
+    helloWorld();
+    std::cout << "This is a simple C++ program." << std::endl;
+    std::cout << "It demonstrates basic syntax and function usage." << std::endl;
+    std::cout << "Feel free to modify and experiment with the code!" << std::endl;
+    std::cout << "Happy coding!" << std::endl;
+    return 0;
+}
